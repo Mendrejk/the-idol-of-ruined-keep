efd890ab7910960491bcf002342ea4d2f45bc511
Mendrejk <bastian.lakomy@gmail.com>
2023-05-07 14:39:07 +0200

    Make card draw happen on turn end

--- game/Scripts/Deck.gd
+++ game/Scripts/Deck.gd
@@ -3,28 +3,30 @@ extends TextureButton
 var cardDatabase: Resource = preload("res://Assets/Cards/CardsDatabase.gd").new()
 
 @export var drawer: Node
+@export var discard_pile: TextureButton
 
 var cards: Array[Card] = _create_populated_deck()
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	scale *= Globals.CardSize / size
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
-func _on_gui_input(event):
-	if Input.is_action_just_released("leftclick"):
-		if not cards.is_empty():
-			drawer.draw_card(cards.pop_back())
+func _on_end_of_turn_pressed():
+	draw()
 
-			if cards.is_empty():
-				disabled = true
 
-func _create_populated_deck() -> Array[Card] :
+func draw():
+	if not cards.is_empty():
+		drawer.draw_card(cards.pop_back())
+
+		if cards.is_empty():
+			disabled = true
+
+
+func _create_populated_deck() -> Array[Card]:
 	# TODO: in the future, this should not be hardcoded
-	var attack_weak = cardDatabase.Cards[cardDatabase.AttackWeak]
+	var attack_weak   = cardDatabase.Cards[cardDatabase.AttackWeak]
 	var attack_normal = cardDatabase.Cards[cardDatabase.AttackNormal]
 	var attack_strong = cardDatabase.Cards[cardDatabase.AttackStrong]
 
--- game/Scripts/Enemy.gd
+++ game/Scripts/Enemy.gd
@@ -1,23 +1,18 @@
 extends MarginContainer
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var CurrentHealth = 10
 var MaxHealth = 10
 var Damage = 1
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	pass
 	#$VBoxContainer/ImageContainer/Image.scale *= $VBoxContainer/ImageContainer.rect_min_size/$VBoxContainer/ImageContainer/Image.texture.get_size()
 	$VBoxContainer/Bar/TextureProgressBar.value = 100
 	$VBoxContainer/Bar/Count/Background/Number.text = str(CurrentHealth)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
 func ChangeBanditHealth(Number):
 	CurrentHealth -= Number
 	$VBoxContainer/Bar/TextureProgressBar.value = 100*CurrentHealth/MaxHealth
